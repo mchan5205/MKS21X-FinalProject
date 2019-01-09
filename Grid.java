1c99d358abd496d4b05fd0e8c9559ddb9794c2fb
Michael Chan <mchan10@stuy.edu>
2019-01-09 10:44:56 -0500

    made changesto size of the grid

--- Grid.java
+++ Grid.java
@@ -1,8 +1,8 @@
 public class Grid{
   private Tile[][] tiles;
   public Grid(Player d){
-    tiles = new Object[50][50];
-    Tile[25][25] = d;
+    tiles = new Object[44][44];
+    Tile[22][22] = d;
   }
   public Tile getTile(int x, int y){
     return tiles[x][y];
