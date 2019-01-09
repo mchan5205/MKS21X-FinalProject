1aa0cec233c4414ef892507c2157a59a7c49bab7
weichangyao <wyao1@stuy.edu>
2019-01-09 11:02:22 -0500

    started player branch

---
+++ Player.java
@@ -0,0 +1,26 @@
+public class Player[
+  private int hP;
+  private int atk;
+  private int exp;
+  private String name;
+
+  public Player(String nam; int health; int attack;) {
+    hP = health;
+    atk = attack;
+    name = name;
+    exp = 0;
+  }
+  public class getName {
+     return name;
+  }
+  public class getExp {
+     return exp;
+  }
+  public class getHP {
+     return hP;
+  }
+  public class getAtk {
+     return atk;
+ }
+
+]
