c43868a790ca7e7bb319acc21a3e17149c42e0d7
Michael Chan <mchan10@stuy.edu>
2019-01-10 10:45:33 -0500

    added Monster.java and basic methods

---
+++ Monster.java
@@ -0,0 +1,20 @@
+public class Monster{
+  private int hP;
+  private int atk;
+  private String name;
+  private int expReward;
+  public Monster(){
+  }
+  public int getExp(){
+    return expReward;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getHP(){
+    return hP;
+  }
+  public int getAtk(){
+    return atk;
+  }
+}
