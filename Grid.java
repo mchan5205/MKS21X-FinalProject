c771d2c14952f5efca0e72adb1a8d4eaeaf25a4a
Michael Chan <mchan10@stuy.edu>
2019-01-13 16:57:26 -0500

    fixed for loop

--- Grid.java
+++ Grid.java
@@ -3,7 +3,7 @@ public class Grid{
   public Grid(Player d){
     tiles = new Tile[27][27];
     for (int i = 0; i < 27; i++){
-      for (int x = 0 ;i < 27; i++){
+      for (int x = 0 ;i < 27; x++){
         tiles[i][x] = new Tile(new Empty(), true);
       }
     }
