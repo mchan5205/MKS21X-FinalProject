d872e4893af95268681d73501c995c29f13c7876
Michael Chan <mchan10@stuy.edu>
2019-01-13 20:59:18 -0500

    made random generation work for all 9 rooms

--- Grid.java
+++ Grid.java
@@ -47,32 +47,34 @@ public class Grid{
     }
     /**/
     Random t = new Random();
-    
-    int height;
-    int width;
-    height = Math.abs(t.nextInt() % 3) + 2;
-    width = Math.abs(t.nextInt() % 3) + 2;
-    System.out.println(height);
-    System.out.println(width);
-    for (int y = 4 - width; y <= 4 + width; y++){
-      tiles[4 + height][y] = new Tile(new Empty(), false);
-      tiles[4 - height][y] = new Tile(new Empty(), false);
-    }
-    for (int x = 4 - height; x <= 4 + height; x++){
-      tiles[x][4 + width] = new Tile(new Empty(), false);
-      tiles[x][4 - width] = new Tile(new Empty(), false);
-    }
-    for (int x = 4 - height + 1; x < 4 + height; x++){
-      for (int y = 4 - width + 1; y < 4 + width; y++){
-        tiles[x][y] = new Tile(new Empty(), true);
+    for(int w = 4; w < 23; w = w + 9){
+      for (int h = 4; h < 23; h = h + 9){
+        int height;
+        int width;
+        height = Math.abs(t.nextInt() % 3) + 2;
+        width = Math.abs(t.nextInt() % 3) + 2;
+        System.out.println(height);
+        System.out.println(width);
+        for (int y = w - width; y <= w + width; y++){
+          tiles[h + height][y] = new Tile(new Empty(), false);
+          tiles[h - height][y] = new Tile(new Empty(), false);
+        }
+        for (int x = h - height; x <= h + height; x++){
+          tiles[x][w + width] = new Tile(new Empty(), false);
+          tiles[x][w - width] = new Tile(new Empty(), false);
+        }
+        for (int x = h - height + 1; x < h + height; x++){
+          for (int y = w - width + 1; y < w + width; y++){
+            tiles[x][y] = new Tile(new Empty(), true);
+          }
+        }
+        tiles[h + height][w] = new Tile(new Empty(), true);
+        tiles[h - height][w] = new Tile(new Empty(), true);
+        tiles[h][w + width] = new Tile(new Empty(), true);
+        tiles[h][w - width] = new Tile(new Empty(), true);
       }
     }
-    tiles[4 + height][4] = new Tile(new Empty(), true);
-    tiles[4 - height][4] = new Tile(new Empty(), true);
-    tiles[4][4 + width] = new Tile(new Empty(), true);
-    tiles[4][4 - width] = new Tile(new Empty(), true);
     tiles[13][13] = new Tile(d, true);
-    tiles[13][13] = new Tile(d, false);
   }
   public Tile getTile(int x, int y){
     return tiles[x][y];
